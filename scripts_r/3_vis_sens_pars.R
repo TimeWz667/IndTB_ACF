78eda5e504b8e0d8d6385bbb4a80e87ec08481ff
TimeWz667 <TimeWz667@gmail.com>
2023-10-13 16:14:26 +0100

    Label pars in sensitivity analysis

--- scripts_r/3_vis_sens_pars.R
+++ scripts_r/3_vis_sens_pars.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(latex2exp)
 
 theme_set(theme_bw() + theme(text = element_text(family = "sans")))
 
@@ -35,6 +36,25 @@ ord_mor <- stats %>% group_by(Par) %>%
   arrange(Par) %>% pull(Par) %>% as.character()
 
 
+lab_pars <- c(
+  "rr_beta_dr" = r"($\rho_{dr}$)", 
+  "r_relapse_td" = r"($\rho_{dr}$)", 
+  "rr_sus_ltbi"= r"($\pi$)",  
+  "r_relapse_st" = r"($\gamma_{st}$)",
+  "r_clear" = r"($\nu_{c}$)",      
+  "r_cs_s" = r"($\delta_{0}$)", 
+  "r_relapse_tc"= r"($\gamma_{tc}$)", 
+  "r_onset"= r"($\sigma$)", 
+  "r_react"= r"($\gamma_{re}$)",
+  "p_entry_pri" = r"($p_{Pr0}$)", 
+  "beta_ds" = r"($\beta_{0}$)",
+  "rr_inf_asym" = r"($\rho_{A}$)",
+  "p_dx_pri" = r"($p_{dx, Pub}$)",
+  "r_cs_c" = r"($\delta_{1}$)",
+  "p_dx_pub" = r"($p_{dx, Pub}$)"
+)
+
+
 g_sens_par_inc <- stats %>% 
   mutate(
     Par = factor(Par, ord_inc),
@@ -49,7 +69,7 @@ g_sens_par_inc <- stats %>%
                      sec.axis = sec_axis(~.+as.numeric(stats[1, "Inc0"]), 
                                          labels = scales::percent, 
                                          name = "Averted incidence, %")) +
-  scale_y_discrete("Parameter") +
+  scale_y_discrete("Parameter", labels = TeX(lab_pars)) +
   scale_fill_discrete("Change in parameter value") +
   expand_limits(x = c(-0.0018, 0.0018)) +
   theme(legend.position = c(1, 0), legend.just = c(1.05, 0))  
@@ -69,7 +89,7 @@ g_sens_par_mor <- stats %>%
                      sec.axis = sec_axis(~.+as.numeric(stats[1, "Mor0"]), 
                                          labels = scales::percent, 
                                          name = "Averted mortality, %")) +
-  scale_y_discrete("Parameter") +
+  scale_y_discrete("Parameter", labels = TeX(lab_pars)) +
   scale_fill_discrete("Change in parameter value") +
   expand_limits(x = c(-0.0025, 0.0025)) +
   theme(legend.position = c(1, 0), legend.just = c(1.05, 0))  
